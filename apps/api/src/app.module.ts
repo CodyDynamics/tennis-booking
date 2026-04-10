cbadb8026347ed4cdc5dc83979c381f0aefb192f
Dylan <dylan@lumberworks.xyz>
2026-04-10 12:05:39 +0700

    chore: intentionally cause build error

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -97,6 +97,7 @@ import { NotificationsModule } from "./notifications/notifications.module";
       imports: [ConfigModule],
       global: true,
       useFactory: (config: ConfigService) => {
+        console.log(a);
         const jwtSecret =
           process.env.JWT_SECRET ||
           config.get<string>("jwt.secret") ||
